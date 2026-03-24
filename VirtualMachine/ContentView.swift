2dc58f04775888d2aae9acda4ea33a00e6c4a613
Trent Shea <trentshea@users.noreply.github.com>
2026-03-24 10:05:54 -0600

    Add Xcode project.

---
+++ VirtualMachine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VirtualMachine
+//
+//  Created by Trent Shea on 2026-03-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VirtualMachine/VirtualMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualMachineApp.swift
+//  VirtualMachine
+//
+//  Created by Trent Shea on 2026-03-23.
+//
+
+import SwiftUI
+
+@main
+struct VirtualMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
